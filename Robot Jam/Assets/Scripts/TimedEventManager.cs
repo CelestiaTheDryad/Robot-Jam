f669598d59bf4233d66ab9485575cf149c58a534
digipen <digipen@dit2981us.DIGIPEN.EDU>
2018-09-09 11:27:14 -0700

    Add some waterable registry. Start work on timed event manager

---
+++ Robot Jam/Assets/Scripts/TimedEventManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedEventManager : MonoBehaviour {
+    protected static TimedEventManager __instance;
+    protected Dictionary<float, Action> __timedfunctions;
+
+    protected void Start() {
+        __instance = this;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        foreach (KeyValuePair<float, Action> i in __timedfunctions) {
+            if (Time.time >= i.Key) {
+                i.Value();
+                // @TODO: Broken, need to fix
+            }
+        }
+	}
+
+    public static TimedEventManager GetInstance () {
+        if (__instance == null) {
+            Debug.LogError("TimedEventManager instance doesn't exist");
+        }
+        return __instance;
+    }
+
+    // Adds a timed event set to go off in _inSeconds seconds
+    public void AddTimedEvent (float _inSeconds, Action _event) {
+        // @TODO: dictionary won't work lol
+    }
+}
--- Robot Jam/Assets/Scripts/Waterable.cs
+++ Robot Jam/Assets/Scripts/Waterable.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class Waterable : MonoBehaviour
 {
-
+    public static List<Waterable> allWaterables;
     public bool Watered = false;
     public float WateringCost = 1.0f;
+    [SerializeField] protected Renderer unwateredStateRenderer;
+    [SerializeField] protected Renderer wateredStateRenderer;
+    [SerializeField] ParticleSystem[] wateredParticles;
 
     // Use this for initialization
     void Start()
     {
-
+        if (allWaterables == null) {
+            allWaterables = new List<Waterable>();
+        }
+        allWaterables.Add(this);
+        EnableRendering (Watered);
     }
 
     // Update is called once per frame
@@ -20,8 +27,35 @@ public class Waterable : MonoBehaviour
 
     }
 
-    public void Water()
+    void EnableRendering (bool _watered) {
+        if (unwateredStateRenderer != null) {
+            unwateredStateRenderer.enabled = !_watered;
+        }
+        if (wateredStateRenderer != null) {
+            wateredStateRenderer.enabled = _watered;
+        }
+    }
+
+    public bool Water()
     {
+        // Play watering animation
+        foreach (ParticleSystem i in wateredParticles) 
+        {
+            i.Play ();
+
+            // @TODO: Build delay period for enabling rendering based on particle state
+        }
+
+        // Play sound
+        //AudioManager.PlaySound("Water");
+
+        // Set state to true
         Watered = true;
+
+        // Rendering
+        EnableRendering(Watered);
+
+        // Return that this was watered
+        return true;
     }
 }
