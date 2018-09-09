a5f78000d1d83e078997505ebbf497a177b20042
digipen <digipen@dit2981us.DIGIPEN.EDU>
2018-09-09 12:45:11 -0700

    Fixed TimedEventManager to use Awake.  Added player model + test animation

---
+++ Robot Jam/Assets/Scripts/PlayerAnimationController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour {
+
+    [SerializeField] protected AnimationController animationController;
+    [SerializeField] protected Animator theAnimator;
+
+	// Use this for initialization
+	void Start () {
+        TimedEventManager.GetInstance().AddTimedEvent(2f, () => {
+            Debug.Log("Attempt transition!");
+            theAnimator.SetTrigger("startWalking");
+        });
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(TimedEventManager.GetInstance());
+	}
+}
